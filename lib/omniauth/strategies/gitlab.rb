faae5be43de203e04161611c0430df3ec3fccb56
Larry Lv <zdlv@freewheel.tv>
2014-12-10 16:07:10 +0800

    Require 'omniauth'.

--- lib/omniauth/strategies/gitlab.rb
+++ lib/omniauth/strategies/gitlab.rb
@@ -1,5 +1,6 @@
 require 'faraday'
 require 'multi_json'
+require 'omniauth'
 
 module OmniAuth
   module Strategies
@@ -19,7 +20,7 @@ module OmniAuth
           options[:on_login].call(self.env)
         else
           form = OmniAuth::Form.new(:title =>  (options[:title] || "Gitlab Verification"), :url => callback_path)
-          
+
           form.text_field 'Email', 'email'
           form.password_field 'Password', 'password'
           form.button "Sign In"
@@ -31,24 +32,24 @@ module OmniAuth
         return fail!(:invalid_credentials) unless identity
         super
       end
-      
+
       uid{ identity['id'].to_s }
       info do
         {
           :name => identity['name'],
           :email => identity['email'],
           :nickname => identity['username']
-        } 
+        }
       end
-      
+
       credentials do
         { :token => identity['private_token'] }
       end
-      
-      extra do 
+
+      extra do
         { :raw_info => identity }
       end
-      
+
       def identity
         @identity ||= begin
           conn = Faraday.new(:url => options[:site])
@@ -58,7 +59,7 @@ module OmniAuth
             req.params = { :email => request['email'], :password => request['password'] }
           end
           resp.success? ? MultiJson.decode(resp.body) : nil
-        end 
+        end
       end
     end
   end
