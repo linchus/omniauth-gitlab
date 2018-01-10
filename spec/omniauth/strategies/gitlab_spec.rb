3f8a11b2af8f26a0c645385c34f9c22124b03f1d
Sergey Sein <linchus@gmail.com>
2018-01-10 23:31:43 +0200

    Cleanup code

--- spec/omniauth/strategies/gitlab_spec.rb
+++ spec/omniauth/strategies/gitlab_spec.rb
@@ -1,24 +1,19 @@
 require 'spec_helper'
 
 describe OmniAuth::Strategies::GitLab do
-
   let(:access_token) { double('AccessToken') }
   let(:parsed_response) { double('ParsedResponse') }
   let(:response) { double('Response', parsed: parsed_response) }
 
-  let(:enterprise_site)           { 'https://some.other.site.com/api/v3' }
-  let(:enterprise_authorize_url)  { '/oauth/authorize' }
-  let(:enterprise_token_url)      { '/oauth/access_token' }
+  let(:enterprise_site) { 'https://some.other.site.com/api/v3' }
 
   let(:gitlab_service) { OmniAuth::Strategies::GitLab.new({}) }
   let(:enterprise) do
-    OmniAuth::Strategies::GitLab.new('GITLAB_KEY', 'GITLAB_SECRET',
-                                         client_options: {
-                                             site: enterprise_site,
-                                             authorize_url: enterprise_authorize_url,
-                                             token_url: enterprise_token_url
-                                         },
-                                         redirect_url: 'http://localhost:9292/callback_url'
+    OmniAuth::Strategies::GitLab.new(
+      'GITLAB_KEY',
+      'GITLAB_SECRET',
+      client_options: { site: enterprise_site },
+      redirect_url: 'http://localhost:9292/callback_url'
     )
   end
 
@@ -32,17 +27,13 @@ describe OmniAuth::Strategies::GitLab do
     context 'with defaults' do
       subject { gitlab_service.options.client_options }
 
-      its(:site) { is_expected.to eq 'https://gitlab.com' }
-      its(:authorize_url) { is_expected.to eq '/oauth/authorize' }
-      its(:token_url) { is_expected.to eq '/oauth/token' }
+      its(:site) { is_expected.to eq 'https://gitlab.com/api/v4' }
     end
 
     context 'with override' do
       subject { enterprise.options.client_options }
 
       its(:site) { is_expected.to eq enterprise_site }
-      its(:authorize_url) { is_expected.to eq enterprise_authorize_url }
-      its(:token_url) { is_expected.to eq enterprise_token_url }
     end
   end
 
@@ -60,7 +51,7 @@ describe OmniAuth::Strategies::GitLab do
 
   describe '#raw_info' do
     it 'sent request to current user endpoint' do
-      expect(access_token).to receive(:get).with('/api/v3/user').and_return(response)
+      expect(access_token).to receive(:get).with('user').and_return(response)
       expect(subject.raw_info).to eq(parsed_response)
     end
   end
